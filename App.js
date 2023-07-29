6b5811dbbae0881ce73f3b7e6d2b0d33e84d4424
ghanshyamtanchak <ghtanchak77@gmail.com>
2023-07-29 17:42:58 +0530

    common component

--- App.js
+++ App.js
@@ -1,2 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./styles.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img src="http://placehold.it" />
+      </div>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">footer</div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="body-container">body</div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <main className="">
+      <Header />
+      <Body />
+      <Footer />
+    </main>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
