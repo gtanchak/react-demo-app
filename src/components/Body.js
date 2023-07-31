b9964c6c21e59ecef4abe8395782373439944fb6
ghanshyamtanchak <ghtanchak77@gmail.com>
2023-07-31 22:24:33 +0530

    api calling setup

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,24 @@
 import restList from "../utils/constants";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const Body = () => {
-  const [restaurantList, setRestaurantList] = useState(restList);
+  const [restaurantList, setRestaurantList] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.210826&lng=72.8580097&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    setRestaurantList(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
   return (
     <div className="body">
       <div className="search">Search</div>
