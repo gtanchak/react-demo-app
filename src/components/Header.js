3659e2e4f21e5905c19d68833cd4926415c09f86
ghanshyamtanchak <ghtanchak77@gmail.com>
2023-08-01 21:03:40 +0530

    added login button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,31 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="logo-container">
-                <img
-                    className="logo"
-                    src={LOGO_URL}
-                />
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
-        </div>
-    );
+  const [btnText, setBtnText] = useState("Login");
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() =>
+              btnText === "Login" ? setBtnText("Logout") : setBtnText("Login")
+            }
+          >
+            {btnText}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-export default Header
+export default Header;
