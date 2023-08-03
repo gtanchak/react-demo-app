68cc48fa2afe4c6e8400c165243a6ff28c786471
ghanshyamtanchak <ghtanchak77@gmail.com>
2023-08-03 23:05:04 +0530

    added layout component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import "./index.css";
 import Body from "./components/Body";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
@@ -13,7 +13,7 @@ const AppLayout = () => {
   return (
     <main className="">
       <Header />
-      <Body />
+      <Outlet />
     </main>
   );
 };
@@ -23,16 +23,22 @@ const appRouter = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Body />
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      }],
     errorElement: <Error />
   },
-  {
-    path: '/about',
-    element: <About />
-  },
-  {
-    path: '/contact',
-    element: <Contact />
-  }
+
 ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [btnText, setBtnText] = useState("Login");
@@ -11,7 +12,9 @@ const Header = () => {
       <div className="nav-items">
         <ul>
           <li>Home</li>
-          <li>About Us</li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
           <li>Contact Us</li>
           <li>Cart</li>
           <button
