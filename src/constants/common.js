3e2f375b313cc430b24d77c174a4e6066ac87515
gtanchak <ghtanchak77@gmail.com>
2023-09-14 23:21:08 +0530

    fix: header style

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,6 +1,3 @@
-export const LOGO_URL =
-  "https://1000logos.net/wp-content/uploads/2016/10/Burger-King-Logo-1999.jpg";
-
 export const FOOD_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
